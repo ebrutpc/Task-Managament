f6f53368504752c33695688d97ab077de7cb5548
ebrut <ebrutpc00@gmail.com>
2024-05-19 20:02:53 +0300

    feat:Add TypeORM configuration

--- src/Infra/databases/postgres.database.ts
+++ src/Infra/databases/postgres.database.ts
@@ -1,12 +1,17 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-export const postgres = TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'task-management',
-  autoLoadEntities: true,
-  synchronize: true,
+export const postgres = TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => ({
+    type: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+  }),
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './domains/tasks/tasks.module';
 import { postgres } from './Infra/databases/postgres.database';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
-  imports: [TasksModule, postgres, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.stage.${process.env.STAGE}`,
+      validationSchema: configValidationSchema,
+    }),
+    TasksModule,
+    postgres,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
